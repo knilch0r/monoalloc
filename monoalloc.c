045981041834bb996f3f61e30557627b036eb204
knilch0r <knilch0r@users.noreply.github.com>
2019-02-07 20:12:28 +0100

    fix debug output, use correct header for sbrk(), add license

--- monoalloc.c
+++ monoalloc.c
@@ -1,8 +1,22 @@
+/* monoalloc - simple monotonic allocator implementation of malloc()
+ * Copyright (C) 2019 knilch - knilch0r@cwde.de
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 #ifdef DEBUG
 #include <stdio.h>
+static int indebug;
 #endif
 /* size of initial allocation */
 size_t monoalloc_size = 50 * 1042 * 1024;
@@ -10,8 +24,6 @@ static int init;
 
 static void *next;
 static size_t currsize;
-
-extern void *sbrk(intptr_t increment);
 
 static void check_init(void) {
 	if (!init) {
@@ -36,7 +48,12 @@ void *malloc(size_t size) {
 	next = (char *)next + size;
 	currsize += size;
 #ifdef DEBUG
-	printf("trace: malloc'd %zd bytes, next %p, return %p\n", size, next, tmp);
+	if (!indebug) {
+		/* printf() would call malloc(), resulting in endless recursion... */
+		indebug = 1;
+		printf("trace: malloc'd %zd bytes, next %p, return %p\n", size, next, tmp);
+		indebug = 0;
+	}
 #endif
 	return tmp;
 }
