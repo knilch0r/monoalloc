00000062305ed6e644964efa3d37b2fc2d90c20c
knilch0r <knilch0r@users.noreply.github.com>
2019-02-07 21:16:04 +0100

    add test framework

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ int main(int argc, const char**argv)
   std::vector<int> b(23,42);
   std::vector<int> c(b);
   c.push_back(argc);
-  a.resize(1024*1024*10*argc);
+  a.resize(1024*1024*5*argc);
   a[1000000] = 5;
   a.resize(1);
 
